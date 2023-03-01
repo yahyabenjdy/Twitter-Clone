2e28db4915fe5f44376c4d37caa0b1980ee6b21c
Yahya Benjdy <yahyabenjdy044@gmail.com>
2023-03-01 14:59:58 +0100

    Update script.js

--- Twitter Clone/script.js
+++ Twitter Clone/script.js
@@ -2,12 +2,29 @@
 const mainPage = document.querySelector(".main-page");
 const loginPage = document.querySelector(".login-page");
 const middleContent = document.querySelector(".middle-content");
+const btnTop = document.querySelector(".btn-top");
+const newsFeedPage = document.querySelector(".feeds-page");
 
 /* ********************************************************* */
 
 // Main Page
 
 const goToLoginPage = () => {
-  mainPage.getElementsByClassName.display = "none";
-  loginPage.getElementsByClassName.display = "grid";
+  mainPage.style.display = "none";
+  loginPage.style.display = "grid";
 };
+
+middleContent.addEventListener("click", (e) => {
+  if (e.target.classList[1] === "main-btn") {
+    goToLoginPage();
+  }
+});
+
+btnTop.addEventListener("click", () => {
+  const inputUserInfo = document.querySelector("user-info");
+  const inputPassword = document.querySelector(".password");
+
+  if (inputUserInfo.value !== "" && inputPassword.value !== "") {
+    mainPage.style.display = "none";
+  }
+});
