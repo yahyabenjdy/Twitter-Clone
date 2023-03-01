8ce5bc3ffcdcfad4489ab69f55e385da26c8a94b
Yahya Benjdy <yahyabenjdy044@gmail.com>
2023-03-01 14:38:17 +0100

    Update script.js

--- Twitter Clone/script.js
+++ Twitter Clone/script.js
@@ -1,2 +1,13 @@
- 
- 
+// DOM Element
+const mainPage = document.querySelector(".main-page");
+const loginPage = document.querySelector(".login-page");
+const middleContent = document.querySelector(".middle-content");
+
+/* ********************************************************* */
+
+// Main Page
+
+const goToLoginPage = () => {
+  mainPage.getElementsByClassName.display = "none";
+  loginPage.getElementsByClassName.display = "grid";
+};
