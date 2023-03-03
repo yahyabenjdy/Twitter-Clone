dd0ef33c1c3b3a3b909038e073f9fbea1077afed
Yahya Benjdy <yahyabenjdy044@gmail.com>
2023-03-03 09:53:21 +0100

    Update script.js

--- Twitter Clone/script.js
+++ Twitter Clone/script.js
@@ -10,6 +10,10 @@ const loginFormBtn = document.querySelector(".login-form-btn");
 const postBtn = document.querySelector(".post-btn");
 const modalWrapper = document.querySelector(".modal-wrapper");
 const modal = document.querySelector(".modal");
+const postModalX = document.querySelector(".modal-header i");
+const modalPostBtn = document.querySelector(".modal-header button");
+const modalFooterPlus = document.querySelector(".modal-footer span");
+const modalInput = document.querySelector(".modal-input");
 
 /* ********************************************************* */
 
@@ -61,4 +65,32 @@ loginFormBtn.addEventListener("click", () => {
 // Post modal
 postBtn.addEventListener("click", () => {
   modal.style.display = "block";
+  modalWrapper.classList.add("modal-wrapper-display");
+
+  if (modalInput.value !== "") {
+    modalInput.value = "";
+    changeOpacity(0.5);
+  }
+});
+
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x;
+  modalFooterPlus.style.opacity = x;
+};
+
+postModalX.addEventListener("click", () => {
+  modal.style.display = "none";
+  modalWrapper.classList.remove("modal-wrapper-display");
+});
+
+modalInput.addEventListener("keypress", (e) => {
+  if (e.target.value !== "") {
+    changeOpacity(1);
+  }
+});
+
+modalInput.addEventListener("blur", (e) => {
+  if (e.target.value === "") {
+    changeOpacity(0.5);
+  }
 });
